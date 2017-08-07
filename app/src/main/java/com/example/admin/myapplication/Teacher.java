d8b7f1225405acfe58051659de2046f8243b0fa9
xyh1993 <381701532@qq.com>
2017-08-07 09:22:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/admin/myapplication/Teacher.java
@@ -0,0 +1,26 @@
+package com.example.admin.myapplication;
+
+/**
+ * Created by admin on 2017-08-04.
+ */
+
+public class Teacher {
+    private int a;
+    private int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
